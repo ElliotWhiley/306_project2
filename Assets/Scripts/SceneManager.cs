e3003a6c974d71d5a00b407ae98a2d6344b9604b
ElliotWhiley <elliot.w@hotmail.com>
2015-10-03 17:48:36 +1300

    added SceneManager to manage flow between screens/levels

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Manages flow between different UI screens and game levels
+public class SceneManager : MonoBehaviour {
+	
+	// Changes current focus to specified scene (find scene by number under file -> Build Settings in Unity)
+	public void ChangeToScene (string sceneToChangeTo) {
+        Application.LoadLevel(sceneToChangeTo);
+	}
+
+    // Exits the game
+    public void QuitGame () {
+        Application.Quit();
+    }
+}
