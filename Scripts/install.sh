48f25ae91b688bb213ca5e306f04a28d65de3819
ElliotWhiley <elliot.w@hotmail.com>
2015-10-10 12:01:37 +1300

    removed unnecessary files

--- Scripts/install.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-# Install script for Unity project
-
-echo 'Downloading from http://netstorage.unity3d.com/unity/44735ea161b3/MacEditorInstaller/Unity-5.2.1f1.pkg: '
-curl -o Unity.pkg http://netstorage.unity3d.com/unity/44735ea161b3/MacEditorInstaller/Unity-5.2.1f1.pkg
-
-echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /S
