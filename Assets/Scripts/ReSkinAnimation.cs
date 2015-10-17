4c0b63b604f1c055aa94f85f1731c957f47ff8db
tota965 <tota965@aucklanduni.ac.nz>
2015-10-17 17:53:14 +1300

    added different skin animations

---
+++ Assets/Scripts/ReSkinAnimation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System;
+
+public class ReSkinAnimation : MonoBehaviour {
+
+	public Animation anim;
+	void Start() {
+		anim = GetComponent<Animation>();
+		foreach (AnimationState state in anim) {
+			state.speed = 0.5F;
+		}
+	}
+
+	
+}
